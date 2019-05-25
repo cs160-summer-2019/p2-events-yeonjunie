6989633b8fa038d41ca13f6ae88fb2c81435c8f2
Andrew Head <head.andrewm@gmail.com>
2019-05-24 17:44:08 -0700

    Update to event handling prototype

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('a/', views.a, name='a'),
+    path('b/', views.b, name='b'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def a(request):
+    return render(request, 'a.html')
+
+def b(request):
+    return render(request, 'b.html')
+
 def index(request):
     return render(request, 'index.html')
