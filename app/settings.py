de50b8df019621ff106cdeac3e8c03bf0a809076
yeon_jun_kim <yeon_jun_kim@brown.edu>
2019-07-08 18:52:28 -0400

    1a updated code

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5u$7%alr4$lntnwcb!u9ne1hkh++s6^=g$rx@yxcy8n-o12&hj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.4", '0.0.0.0', "127.0.0.1" ]
+ALLOWED_HOSTS = ["192.168.1.4", '0.0.0.0', "127.0.0.1", "p2-events-yeonjunie-yeonjunkim915170.codeanyapp.com"]
 
 
 # Application definition
